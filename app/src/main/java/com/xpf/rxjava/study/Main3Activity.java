8de1ae48f22ca7dfd6ceb6ae1694b178c9122c77
jiushu <xpfwork@foxmail.com>
2020-11-21 16:24:10 +0800

    变换型操作符

--- app/src/main/java/com/xpf/rxjava/study/Main3Activity.java
+++ app/src/main/java/com/xpf/rxjava/study/Main3Activity.java
@@ -31,7 +31,7 @@ public class Main3Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main);
 
     }
 
