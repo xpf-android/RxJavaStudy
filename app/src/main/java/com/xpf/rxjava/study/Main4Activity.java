8d3c77e5309d2c115051181d73646307619e3af2
jiushu <xpfwork@foxmail.com>
2020-11-21 08:07:29 +0800

    过滤型操作符

--- app/src/main/java/com/xpf/rxjava/study/Main4Activity.java
+++ app/src/main/java/com/xpf/rxjava/study/Main4Activity.java
@@ -33,7 +33,6 @@ public class Main4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-
     }
 
     /**
