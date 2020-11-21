fda454ea65c70acf2bebbb02516715bb4d0f1b79
jiushu <xpfwork@foxmail.com>
2020-11-21 16:23:10 +0800

    创建型操作符

--- app/src/main/java/com/xpf/rxjava/study/Main2Activity.java
+++ app/src/main/java/com/xpf/rxjava/study/Main2Activity.java
@@ -20,7 +20,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
     }
 
     /**
