44604ec1dc78db999421292f11696a643df78432
jiushu <xpfwork@foxmail.com>
2020-11-26 20:47:53 +0800

    泛型

---
+++ app/src/main/java/com/xpf/rxjava/study/genericity/A.java
@@ -0,0 +1,44 @@
+package com.xpf.rxjava.study.genericity;
+
+import android.util.Log;
+
+/**
+ * 泛型方法要注意的问题
+ * @param <T>
+ */
+public class A<T> {
+
+    //我想说的其实是这个，虽然在方法中使用了泛型，但是这并不是一个泛型方法。
+    //这只是类中一个普通的成员方法，只不过他的返回值是在声明泛型类已经声明过的泛型。
+    //所以在这个方法中才可以继续使用 T 这个泛型。
+    public T getKey(T t) {
+        return t;
+    }
+
+
+    //这也不是一个泛型方法，这就是一个普通的方法，只是使用了Generic<Number>这个泛型类做形参而已。
+    public void showKeyValue1(A<Number> a){
+        Log.d("泛型测试","key value is " + a.show2());
+    }
+
+    //泛型方法
+    public <E> E show(E e) {
+
+        return e;
+    }
+
+
+    public String show2() {
+        return "";
+    }
+
+    /**
+     * 如果在类中定义使用泛型的静态方法，需要添加额外的泛型声明（将这个方法定义成泛型方法）
+     * 即使静态方法要使用泛型类中已经声明过的泛型也不可以。
+     * 如：public static void show(T t){..},此时编译器会提示错误信息：
+     "StaticGenerator cannot be refrenced from static context"
+     */
+    public static <T> void test(T t){
+
+    }
+}
